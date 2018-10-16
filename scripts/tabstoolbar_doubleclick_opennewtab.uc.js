85ef4c51e32a4dd7868c80311e9934e03d5659ca
Aris-t2 <aris-addons@gmx.net>
2018-10-16 19:26:54 +0200

    Update scripts/tabstoolbar_doubleclick_opennewtab.uc.js

---
+++ scripts/tabstoolbar_doubleclick_opennewtab.uc.js
@@ -0,0 +1,29 @@
+// 'Double click on tabs toolbar opens a new tab' script for Firefox 60+ by Aris
+
+var NewTabOnDoubleClick = {
+  init: function() {
+
+	document.getElementById("TabsToolbar").addEventListener("dblclick", function openNewTabOnDoubleClick(e) {
+				
+	  if(e.button==0
+	    && e.target.localName != "tab"
+		  && e.target.localName != "toolbarbutton"
+			&& e.target.localName != "arrowscrollbox"
+			  && e.originalTarget.getAttribute("anonid") != "scrollbutton-up"
+				&& e.originalTarget.getAttribute("anonid") != "scrollbutton-down"
+				  && e.originalTarget.getAttribute("anonid") != "close-button")
+	  {
+
+		BrowserOpenTab();
+		e.stopPropagation();
+		e.preventDefault();
+
+	  }
+
+	}, false);
+
+  }
+
+}
+
+NewTabOnDoubleClick.init();
