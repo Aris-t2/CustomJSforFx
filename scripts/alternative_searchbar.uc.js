f2d0a5fd43d067988fae0a2d0b413f86abdca531
Aris-t2 <aris-addons@gmx.net>
2018-12-29 18:02:41 +0100

    Update alternative_searchbar.uc.js

--- scripts/alternative_searchbar.uc.js
+++ scripts/alternative_searchbar.uc.js
@@ -1,7 +1,6 @@
-// 'Alternative search bar' script for Firefox 64+ by Aris
-// (script does not work with Firefox versions lower than 64)
+// 'Alternative search bar' script for Firefox 60+ by Aris
 //
-// Based on 'search revert' script by 2002Andreas:
+// Based on 'search revert' script by '2002Andreas':
 // https://www.camp-firefox.de/forum/viewtopic.php?f=16&t=112673&start=2010#p1099758
 //
 // Search glass always visible at search bars end (like with old search)
@@ -10,20 +9,34 @@
 //
 // option: clear search input after search
 // option: revert to first search engine in list after search
+// option: old search engine selection popup (FIREFOX 64+ only!)
 // option: hide oneoff search engines
+// option: show search engine names instead of icons only
 // option: select search engine by scrolling mouse wheel over searchbars button
 //
-// Not present: old popup to switch engines !
-//
-// Note: 'Ctrl + Up&Down keys' and 'Ctrl + Mouse wheel up&down' combos switch through search engines
+// [!] Old search engine selection popup: 'add engines' feature not present!
+// [!] If searchbar is focused, what can be done with 'Ctrl + K' or 'Ctrl + E', one can switch
+//     through search engines with 'Ctrl + Up&Down keys' and 'Ctrl + Mouse wheel up&down' combos! 
 
+
+// Configuration area - start
 var clear_searchbar_after_search = false; // clear input after search (true) or not (false)
 var revert_to_first_engine_after_search = false; // revert to first engine (true) or not (false)
-var hide_oneoff_search_engines = false; // hide 'one off' search engines (true) or not (false)
+var old_search_engine_selection_popup_fx64 = true; // show old search engine selection popup (true) or not (false)
 var select_engine_by_scrolling_over_button = false; // select search engine by scrolling mouse wheel over searchbars button (true) or not (false)
+var hide_oneoff_search_engines = false; // hide 'one off' search engines (true) or not (false)
+var show_search_engine_names = false; // show search engine names (true) or not (false)
+var show_search_engine_names_with_scrollbar = false; // show search engine names with scrollbars (true) or not (false)
+var show_search_engine_names_with_scrollbar_height = '170px'; // higher values show more search engines
+var searchsettingslabel = "Search Settings";
+// Configuration area - end
 
+
+
+// main function
 (function() {
 	var searchbar = document.getElementById("searchbar");
+	var appversion = parseInt(Services.appinfo.version);
 	
 	updateStyleSheet();
 	
@@ -34,9 +47,109 @@ var select_engine_by_scrolling_over_button = false; // select search engine by s
           searchbar.selectEngine(event, event.detail > 0);
 		}
 	  }, true);
+	  
+	if(old_search_engine_selection_popup_fx64 && appversion >= 64) {
+
+	  var engines = searchbar.engines;
+		
+	  // set new search engine
+	  searchbar.setNewSearchEngine = function(index) {
+		searchbar.currentEngine = searchbar.engines[index];
+	  };
+
+	  // create search popup
+	  if(appversion <= 62) searchbuttonpopup = document.createElement("menupopup");
+		else searchbuttonpopup = document.createXULElement("menupopup");
+	  searchbuttonpopup.setAttribute("id", "searchbuttonpopup");
+	  searchbuttonpopup.setAttribute("width", searchbar.firstChild.nextSibling.getBoundingClientRect().width - 6 );
+	  searchbuttonpopup.setAttribute("position", "after_start");
+  
+	  for (var i = 0; i <= engines.length - 1; ++i) {
+					
+		if(appversion <= 62) menuitem = document.createElement("menuitem");
+		  else menuitem = document.createXULElement("menuitem");
+		var name = engines[i].name;
+		menuitem.setAttribute("label", name);
+		menuitem.setAttribute("class", "menuitem-iconic searchbar-engine-menuitem menuitem-with-favicon");		
+		if (engines[i] == searchbar.currentEngine)
+		  menuitem.setAttribute("selected", "true");
+		if (engines[i].iconURI)
+		  searchbar.setIcon(menuitem, engines[i].iconURI.spec);
+
+		menuitem.setAttribute("oncommand", "document.getElementById('searchbar').setNewSearchEngine("+i+")");
+		
+		searchbuttonpopup.appendChild(menuitem);
+
+	  }
+
+	  if(appversion <= 62) menuitem = document.createElement("menuseparator");
+		else menuseparator = menuseparator_om = document.createXULElement("menuseparator");
+	  searchbuttonpopup.appendChild(menuseparator_om);
+		
+	  if(appversion <= 62) menuitem_om = document.createElement("menuitem");
+		else menuitem_om = document.createXULElement("menuitem");
+	  menuitem_om.setAttribute("label", searchsettingslabel);
+	  menuitem_om.setAttribute("class", "open-engine-manager");	
+	  menuitem_om.setAttribute("oncommand", "openPreferences('search');");
+	  searchbuttonpopup.appendChild(menuitem_om);
+	
+	  document.getElementById("mainPopupSet").appendChild(searchbuttonpopup);
+  
+	  // attach new popup to searchbars search button
+	  document.getAnonymousElementByAttribute(searchbar.firstChild.nextSibling, "class", "searchbar-search-button").setAttribute("popup", "searchbuttonpopup");
+		  
+	  // hide default popup, if clicking on search button
+	  searchbar.addEventListener("mousedown", (event) => {
+	   if (event.originalTarget.classList.contains("searchbar-search-button")) {
+	
+		document.getElementById('PopupSearchAutoComplete').hidePopup();
+		document.getElementById("PopupSearchAutoComplete").style.visibility="collapse";
+		
+		setTimeout(function() {
+		 document.getElementById("PopupSearchAutoComplete").style.visibility="visible";
+		 document.getElementById('PopupSearchAutoComplete').hidePopup();
+		}, 1000);
+		  
+	   }
+	  }, true);
+
+	};
+	  
+	// doSearch function taken from browsers internal 'searchbar.xml' file and modified
+	if(appversion < 63) searchbar.doSearch = function(aData, aWhere, aEngine) {
+			
+      var textBox = this._textbox;
+	  
+      if (aData && !PrivateBrowsingUtils.isWindowPrivate(window)) {
+         this.FormHistory.update(
+           { op : "bump",
+             fieldname : textBox.getAttribute("autocompletesearchparam"),
+             value : aData },
+           { handleError : function(aError) {
+               Components.utils.reportError("Saving search to form history failed: " + aError.message);
+           }});
+       }
+
+       let engine = aEngine || this.currentEngine;
+       var submission = engine.getSubmission(aData, null, "searchbar");
+       let params = {
+         postData: submission.postData,
+         inBackground: aWhere == "tab-background"
+       };
+
+       openUILinkIn(submission.uri.spec,aWhere == "tab-background" ? "tab" : aWhere,params);
+
+	  if(clear_searchbar_after_search)
+		this.value = '';
+		  
+	  if(revert_to_first_engine_after_search) {
+		  this.currentEngine = this.engines ? this.engines[0] : this._engines[0];
+		  updateStyleSheet();
+	  }
+	};
 	
 	// doSearch function taken from browsers internal 'searchbar.js' file and modified
-	searchbar.doSearch = function(aData, aWhere, aEngine, aParams, aOneOff) {
+	if(appversion >= 63) searchbar.doSearch = function(aData, aWhere, aEngine, aParams, aOneOff) {
 			
 	  let textBox = this._textbox;
 
@@ -86,18 +199,38 @@ var select_engine_by_scrolling_over_button = false; // select search engine by s
 			updateStyleSheet();
 		}
 	};
+
 		
 	// setIcon function taken from browsers internal 'searchbar.js' file and modified
 	searchbar.setIcon = function(element, uri) {
 	  element.setAttribute("src", uri);
 	  updateStyleSheet();
-	}
+	};
+
 	
+	// override selectEngine function and remove automatic popup opening
+	searchbar.selectEngine = function(aEvent, isNextEngine) {
+      // Find the new index
+      let newIndex = this.engines.indexOf(this.currentEngine);
+      newIndex += isNextEngine ? 1 : -1;
+
+      if (newIndex >= 0 && newIndex < this.engines.length) {
+        this.currentEngine = this.engines[newIndex];
+      }
+
+      aEvent.preventDefault();
+      aEvent.stopPropagation();
+
+	};
+	
+
 	// main style sheet
 	function updateStyleSheet() {
 	  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
 	  
 	  var hide_oneoff_search_engines_code='';
+	  var show_search_engine_names_code='';
+	  var show_search_engine_names_with_scrollbar_code='';
 	
 	  if(hide_oneoff_search_engines)
 		hide_oneoff_search_engines_code=' \
@@ -106,9 +239,65 @@ var select_engine_by_scrolling_over_button = false; // select search engine by s
 			display: none !important; \
 		  } \
 		';
+		
+	  if(show_search_engine_names && !hide_oneoff_search_engines)
+	   show_search_engine_names_code=' \
+		#PopupSearchAutoComplete .search-panel-tree { \
+		  display: block !important; \
+		  width: 100% !important; \
+		} \
+		#PopupSearchAutoComplete .search-panel-tree *[anonid="main-box"] { \
+		  width: 100%; \
+		} \
+		#PopupSearchAutoComplete .search-panel-one-offs .searchbar-engine-one-off-item { \
+		  -moz-appearance:none !important; \
+		  min-width: 0 !important; \
+		  width: 100% !important; \
+		  border: unset !important; \
+		  height: 22px !important; \
+		  background-image: unset !important; \
+		  -moz-padding-start: 3px !important; \
+		} \
+		#PopupSearchAutoComplete .search-panel-one-offs .searchbar-engine-one-off-item:not([tooltiptext]) { \
+		  display: none !important; \
+		} \
+		#PopupSearchAutoComplete .search-panel-one-offs .searchbar-engine-one-off-item .button-box { \
+		  position: absolute !important; \
+		  -moz-padding-start: 4px !important; \
+		  margin-top: 3px !important; \
+		} \
+		#PopupSearchAutoComplete .search-panel-one-offs .searchbar-engine-one-off-item::after { \
+		  -moz-appearance: none !important; \
+		  display: inline !important; \
+		  content: attr(tooltiptext) !important; \
+		  position: relative !important; \
+		  top: -9px !important; \
+		  -moz-padding-start: 25px !important; \
+		  min-width: 0 !important; \
+		  width: 100% !important; \
+		  white-space: nowrap !important; \
+		} \
+		';
+		
+	  if(show_search_engine_names_with_scrollbar && !hide_oneoff_search_engines && show_search_engine_names)
+	   show_search_engine_names_with_scrollbar_code=' \
+		#PopupSearchAutoComplete .search-one-offs { \
+		  height: '+show_search_engine_names_with_scrollbar_height+' !important; \
+		  max-height: '+show_search_engine_names_with_scrollbar_height+' !important; \
+		  overflow-y: scroll !important; \
+		  overflow-x: hidden !important; \
+		} \
+		\
+		';
 	  
 	  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(' \
 		\
+		#searchbuttonpopup {\
+		  -moz-margin-start: 3px; \
+		} \
+		.searchbar-search-button { \
+		  -moz-margin-start: -4px; \
+		} \
 		.searchbar-search-button .searchbar-search-icon { \
 		  list-style-image: url('+document.getElementById("searchbar").currentEngine.iconURI.spec+') !important; \
 		} \
@@ -137,7 +326,13 @@ var select_engine_by_scrolling_over_button = false; // select search engine by s
 		  width: 8px !important; \
 		  height: 8px !important; \
 		} \
+		.searchbar-search-button[addengines=true] > .searchbar-search-icon-overlay { \
+		  margin-inline-start: -9px !important; \
+		  margin-inline-end: 2px !important; \
+		} \
 		'+hide_oneoff_search_engines_code+' \
+		'+show_search_engine_names_code+' \
+		'+show_search_engine_names_with_scrollbar_code+' \
 		\
 	  '), null, null);
 	  
