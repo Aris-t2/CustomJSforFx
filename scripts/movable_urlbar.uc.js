6e37297edc6a30b90777b67176a855f81b1c3be2
Aris-t2 <aris-addons@gmx.net>
2019-04-15 10:15:56 +0200

    Update movable_urlbar.uc.js

--- scripts/movable_urlbar.uc.js
+++ scripts/movable_urlbar.uc.js
@@ -3,6 +3,27 @@
 var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
 var {CustomizableUI} = Components.utils.import("resource:///modules/CustomizableUI.jsm", {});
 
+var navigation = CustomizableUI.AREA_NAVBAR;
+var tabs = CustomizableUI.AREA_TABSTRIP;
+var menu = CustomizableUI.AREA_MENUBAR;
+var bookmarks = CustomizableUI.AREA_BOOKMARKS;
+
+
+var urlbar_on_toolbar = navigation;  /* target toolbar of urlbar item*/
+/*	menu = 'menubar'
+	tabs = 'tabs toolbar'
+	bookmarks = 'bookmarks toolbar'
+	navigation='navigation toolbar'
+*/
+
+var urlbar_on_toolbar_position = 2;  /* target position of urlbar item*/
+/*	0 = 1st
+	1 = 2nd
+	2 = 3rd
+	...
+	x = xth
+*/
+
 var MovableUrlbar = {
   init: function() {
 
@@ -10,29 +31,15 @@ var MovableUrlbar = {
 	  document.getElementById('urlbar-container').setAttribute('removable','true');
 	} catch(e){}
 
-	try {
-	  if(!document.getElementById('urlbar-container').getAttribute('cui-areatype'))
-		document.getElementById('urlbar-container').setAttribute('cui-areatype','toolbar');
-	} catch(e){}
-	
-  	var observer = new MutationObserver(function(mutations) {
-	  mutations.forEach(function(mutation) {
-		try {
-		  if(!document.getElementById('urlbar-container').getAttribute('cui-areatype')) {
-			CustomizableUI.addWidgetToArea("urlbar-container", CustomizableUI.AREA_NAVBAR);
-		  } else if (document.getElementById('urlbar-container').getAttribute('cui-areatype')=="menu-panel") {
-			CustomizableUI.addWidgetToArea("urlbar-container", CustomizableUI.AREA_NAVBAR);
-		  }
-		} catch(e){}
-	  });    
-	});
+	CustomizableUI.addWidgetToArea("urlbar-container", urlbar_on_toolbar);
+	CustomizableUI.moveWidgetWithinArea("urlbar-container", urlbar_on_toolbar_position);
 	
 	try {
-	  observer.observe(document.getElementById('urlbar-container'), { attributes: true, attributeFilter: ['cui-areatype'] });
+	  document.getElementById('urlbar-container').setAttribute('removable','false');
 	} catch(e){}
 
   }
 
 }
 
-MovableUrlbar.init();
+document.addEventListener("DOMContentLoaded", MovableUrlbar.init(), false);
