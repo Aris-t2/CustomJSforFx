36a96306ae03091813b7075e097395037a99a2e6
Aris-t2 <aris-addons@gmx.net>
2019-04-19 18:24:07 +0200

    Update favicon_in_urlbar.uc.js

--- scripts/favicon_in_urlbar.uc.js
+++ scripts/favicon_in_urlbar.uc.js
@@ -1,24 +1,24 @@
 // 'Favicon in urlbars identity box' script for Firefox 60+ by Aris
 //
-// The script restores current pages favicon inside location bar.
-// If a page does not offer an icon, branches default icon is used.
+// This script restores current pages favicon inside urlbar (aka location bar, address bar or awesome bar).
+// [!] If a page does not offer a favicon, browser branches default icon is shown.
+// [!] In a multi-window environment pages without favicons might show wrong icons.
 
 
 var FaviconInUrlbar = {
  init: function() {
 
-  try {
-
-	updateStyleSheet();
-
 	// update script every time tab attributes get modified (switch/open tabs/windows)
-	document.addEventListener("TabAttrModified", function update_title() {
-	  updateStyleSheet();
-	}, false);
+	document.addEventListener("TabAttrModified", updateIcon, false);
+	document.addEventListener('TabSelect', updateIcon, false);
+	document.addEventListener('TabOpen', updateIcon, false);
+	document.addEventListener('TabClose', updateIcon, false);
+	document.addEventListener('load', updateIcon, false);
+	document.addEventListener("DOMContentLoaded", updateIcon, false);
 
-	// main style sheet
-	function updateStyleSheet() {
-	  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+	function updateIcon() {
+		
+	 setTimeout(function(){ // timeout fixes wrong icon detection in some cases
 	  
 	  // get current tabs favicon
 	  var favicon_in_urlbar = gBrowser.selectedTab.image;
@@ -26,30 +26,13 @@ var FaviconInUrlbar = {
 	  // if current tab offers no icon, use default branch icon
 	  if(!gBrowser.selectedTab.image || gBrowser.selectedTab.image == null)
 		favicon_in_urlbar = 'chrome://branding/content/identity-icons-brand.svg';
-	
-	  // add tab icon and make sure browsers CSS modifications are disabled 
-	  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(' \
-		#identity-icon, #page-proxy-favicon { \
-		  list-style-image: url('+favicon_in_urlbar+') !important; \
-		  filter: unset !important; \
-		  fill: unset !important; \
-		  width: 16px !important; \
-		  height: 16px !important; \
-		  opacity: 1.0 !important; \
-		  box-shadow: unset !important; \
-		  border-radius: 0px !important; \
-		} \
-		\
-	  '), null, null);
+	  
+	  document.querySelector('#identity-icon').style.listStyleImage = "url("+favicon_in_urlbar+")";
+	  document.querySelector('#page-proxy-favicon').style.listStyleImage = "url("+favicon_in_urlbar+")";
+	  
+	 },100);
 
-	  // remove old style sheet, before registering the new one
-	  if (sss.sheetRegistered(uri,sss.AGENT_SHEET)) { sss.unregisterSheet(uri,sss.AGENT_SHEET); }
-	  sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
-
-	};
-
-  } catch(e) {}
-
+	}
 
  }
 };
