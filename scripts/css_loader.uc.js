8087368ab895033081ac2cd2050288d3f0b4cc42
Aris-t2 <aris-addons@gmx.net>
2019-05-02 10:59:24 +0200

    Create css_loader.uc.js

---
+++ scripts/css_loader.uc.js
@@ -0,0 +1,24 @@
+// 'CSS_Loader' script for Firefox 60+ by Aris
+
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+// replace comments with custom css code
+var my_code = `
+	/* Paste any CSS code here. */
+	/* Make your code is not inside any "@-moz-document / url / url-prefix" queries or it might not work. */
+	`;
+
+var CSS_Loader = {
+ init: function() {
+	
+	var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+	var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(my_code), null, null);
+
+	// remove old style sheet
+	if (sss.sheetRegistered(uri,sss.AGENT_SHEET)) sss.unregisterSheet(uri,sss.AGENT_SHEET);
+	sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+
+ }
+};
+
+CSS_Loader.init();
