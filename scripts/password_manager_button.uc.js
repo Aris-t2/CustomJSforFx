c32ec39086dd33733c5ce6f526b899b38533b913
Aris-t2 <aris-addons@gmx.net>
2020-02-19 17:41:35 +0100

    Update password_manager_button.uc.js

--- scripts/password_manager_button.uc.js
+++ scripts/password_manager_button.uc.js
@@ -2,6 +2,8 @@
 //
 // option: 'leftclick_opens_old_pw_manager' (true) or the new one (false)
 // option: 'middleclick_opens_old_pw_manager' (true) or the new one (false)
+// option: 'aboutlogins_opens_in_new_window' (true) or not (false)
+//
 // default: left-click on password manager button opens new password manager in a tab and adds current (non-chrome) domain/host into input field
 // default: middle-click on password manager button opens 'chrome://passwordmgr/content/passwordManager.xul/xhtml' in a popup
 
@@ -16,6 +18,7 @@ try {
   
   var leftclick_opens_old_pw_manager = false;
   var middleclick_opens_old_pw_manager = true;
+  var aboutlogins_opens_in_new_window = false;
   
   CustomizableUI.createWidget({
 	id: "pw_manager_button", // button id
@@ -25,40 +28,59 @@ try {
 	tooltiptext: "Open Password Manager", // tooltip title
 	onClick: function(event) {
 		
-	  var old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xhtml';
-	  if(appversion < 73) old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xul';
+	  // open about:logins in a new window with custom size
+	  function open_aboutlogins_in_window() {
+		try {
+		  window.open('about:logins' ,'', 'chrome, resizable,width=700,height=500', "");
+		} catch (e) {}
+	  }
+	  
+	  // open about:logins in a new tab and add current domain to it (excl. about pages)
+	  function open_aboutlogins_in_tab() {
+		try {
+		  LoginHelper.openPasswordManager(window, { filterString: gBrowser.currentURI.host, entryPoint: 'mainmenu' });
+		} catch (e) {
+		  LoginHelper.openPasswordManager(window, { entryPoint: 'mainmenu' });
+		}
+	  }
+	  
+	  // open old password manager window
+	  function open_old_pw_manager() {
+		try { 
+		  var old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xhtml';
+		  if(appversion < 73) old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xul';
+		
+		  window.open(old_pw_manager ,'', 'chrome, resizable', "width=400,height=400");
+		} catch (e) {}
+	  }
   
+	  
+	  // left-click
 	  if(event.button=='0') {
 		
 		if(leftclick_opens_old_pw_manager)
-		  try {
-			window.open(old_pw_manager ,'', 'chrome, resizable', "width=400,height=400");
-		  } catch (e) {}
+		  open_old_pw_manager()
 		else {
-		  try {
-			LoginHelper.openPasswordManager(window, { filterString: gBrowser.currentURI.host, entryPoint: 'mainmenu' });
-		  } catch (e) {
-			LoginHelper.openPasswordManager(window, { entryPoint: 'mainmenu' });
-		  }
+		  if(aboutlogins_opens_in_new_window) open_aboutlogins_in_window();
+		  else open_aboutlogins_in_tab();
+		  
 		}
 
-	  } else if(event.button=='1') {
+	  } 
+	  // right-click
+	  else if(event.button=='1') {
 		
 		if(middleclick_opens_old_pw_manager)
-		  try {
-			window.open(old_pw_manager ,'', 'chrome, resizable', "width=400,height=400");
-		  } catch (e) {}
+		  open_old_pw_manager()
 		else {
-		  try {
-			LoginHelper.openPasswordManager(window, { filterString: gBrowser.currentURI.host, entryPoint: 'mainmenu' });
-		  } catch (e) {
-			LoginHelper.openPasswordManager(window, { entryPoint: 'mainmenu' });
-		  }
+		  if(aboutlogins_opens_in_new_window) open_aboutlogins_in_window();
+		  else open_aboutlogins_in_tab();
 		}
 
 	  }
 	  
 	},
+	
 	onCreated: function(button) {
 	  return button;
 	}
