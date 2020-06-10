fc72636514b0c468869b74b9c436f6416379d0fb
Aris-t2 <aris-addons@gmx.net>
2020-06-10 17:03:28 +0200

    Update search_engine_icon_in_searchbar.uc.js

--- scripts/search_engine_icon_in_searchbar.uc.js
+++ scripts/search_engine_icon_in_searchbar.uc.js
@@ -1,24 +1,36 @@
 // 'Search engine icon in search bar' script for Firefox 60+ by Aris
 //
 // Feature: search button shows current search engines icon (like with old search)
-// base on 'alternative_searchbar.uc.js'
+// based on 'alternative_searchbar.uc.js'
+// Fx 77+ fix provided by anomiex
 
-setTimeout(function(){
+var tries = 30;
+var init = function() {
+  // Sometimes search interface is not being created in time. Retry (up to 30 times) until it does.
+  try {
+	document.getElementById("searchbar").currentEngine;
+  } catch(e) {
+	if (--tries > 0) {
+	  setTimeout(init, 1000);
+	}
+  }
+
   try {
 	var searchbar = document.getElementById("searchbar");
 
 	updateStyleSheet();
 
-	// setIcon function taken from browsers internal 'searchbar.js' file and added modifications
-	searchbar.setIcon = function(element, uri) {
-	  element.setAttribute("src", uri);
+	// Override updateDisplay() from browsers internal 'searchbar.js' file to also update the icon
+	var oldUpdateDisplay = searchbar.updateDisplay;
+	searchbar.updateDisplay = function() {
+	  oldUpdateDisplay.call(this);
 	  updateStyleSheet();
 	};
 
 	// main style sheet
 	function updateStyleSheet() {
 	  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
-	
+
 	  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(' \
 		.searchbar-search-button .searchbar-search-icon { \
 		  list-style-image: url('+document.getElementById("searchbar").currentEngine.iconURI.spec+') !important; \
@@ -33,5 +45,5 @@ setTimeout(function(){
 	};
 
   } catch(e) {}
-
-},1000);
+}
+setTimeout(init, 1000);
