fcdb0229d395e5c004f404f008c6651f1ba8e05c
Aris-t2 <aris-addons@gmx.net>
2021-12-21 19:00:58 +0100

    Update tab_label_in_urlbar.uc.js

--- scripts/tab_label_in_urlbar.uc.js
+++ scripts/tab_label_in_urlbar.uc.js
@@ -3,11 +3,19 @@
 (function() {
 	
   try {
-  
+
+	var tablabelbox = document.createXULElement("hbox");
+	tablabelbox.setAttribute("id","tab_label_in_urlbar_box");
+	tablabelbox.setAttribute("class","urlbar-page-action");
+	tablabelbox.setAttribute("role","button");
+
 	var tablabel = document.createXULElement("label");
 	tablabel.setAttribute("id","tab_label_in_urlbar");
-	tablabel.setAttribute("value",gBrowser.selectedBrowser.contentTitle);
-	document.getElementById("page-action-buttons").insertBefore(tablabel, document.getElementById("page-action-buttons").firstChild);
+	tablabelbox.appendChild(tablabel);
+	document.getElementById("page-action-buttons").insertBefore(tablabelbox, document.getElementById("page-action-buttons").firstChild);
+	//document.getElementById("urlbar-input-container").insertBefore(tablabelbox, document.getElementById("urlbar-input-container").querySelector(".urlbar-input-box").nextSibling);
+
+	updateLabel();
   
 	// catch cases where tab title can change
 	document.addEventListener("TabAttrModified", updateLabel, false);
@@ -20,6 +28,26 @@
 	function updateLabel() {
 	  tablabel.setAttribute("value",gBrowser.selectedBrowser.contentTitle);
 	}
+	
+	var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+	var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+	
+	var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
+	  \
+		/* hide item to interact with urlbar, if focused, open and on hover */ \
+		#urlbar:is([focused="true"],[breakout-extend],:hover) #tab_label_in_urlbar_box {\
+		  visibility: collapse !important; \
+		}\
+		\
+		#tab_label_in_urlbar {\
+		  margin-block: unset !important;\
+		  margin-inline: unset !important;\
+		  margin-top: -2px !important;\
+		}\
+	  \
+	'), null, null);
+  
+	sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
   
   } catch(e) {}
 	
