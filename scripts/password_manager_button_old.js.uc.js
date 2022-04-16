a944a64d3bb3258f869ec8826f590174261a34df
Aris-t2 <aris-addons@gmx.net>
2022-04-16 13:58:12 +0200

    pw manager button update

--- scripts/password_manager_button_old.js.uc.js
+++ scripts/password_manager_button_old.js.uc.js
@@ -1,4 +1,10 @@
-// 'Open Password Manager' button for Firefox 91+ by Aris
+// 'Open Password Manager' button for Firefox 60+ by Aris
+//
+// option: 'leftclick_opens_old_pw_manager' (true) or the new one (false)
+// option: 'middleclick_opens_old_pw_manager' (true) or the new one (false)
+//
+// default: left-click on password manager button opens new password manager in a tab and adds current (non-chrome) domain/host into input field
+// default: middle-click on password manager button opens 'chrome://passwordmgr/content/passwordManager.xul/xhtml' in a popup
 
 (function() {
 
@@ -9,6 +15,9 @@ try {
   var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
   var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
   
+  var leftclick_opens_old_pw_manager = false;
+  var middleclick_opens_old_pw_manager = true;
+  
   var appversion = parseInt(Services.appinfo.version);
   
   CustomizableUI.createWidget({
@@ -18,15 +27,44 @@ try {
 	label: "Open Password Manager", // button title
 	tooltiptext: "Open Password Manager", // tooltip title
 	onClick: function(event) {
-
-	  if(event.button=='0') {
+		
+	  
+  
+	  // open about:logins in a new tab and add current domain to it (excl. about pages)
+	  function open_aboutlogins_in_tab() {
 		try {
 		  LoginHelper.openPasswordManager(window, { filterString: gBrowser.currentURI.host, entryPoint: 'mainmenu' });
 		} catch (e) {
 		  LoginHelper.openPasswordManager(window, { entryPoint: 'mainmenu' });
 		}
 	  }
+	  
+	  // open old password manager window
+	  function open_old_pw_manager() {
+		try { 
+		  var old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xhtml';
+		  if(parseInt(Services.appinfo.version) < 73) old_pw_manager = 'chrome://passwordmgr/content/passwordManager.xul';
+		
+		  window.open(old_pw_manager ,'', 'chrome, resizable', "width=400,height=400");
+		} catch (e) {}
+	  }
+  
+	  
+	  // left-click
+	  if(event.button=='0') {
+		
+		if(leftclick_opens_old_pw_manager && appversion < 77) open_old_pw_manager();
+		else open_aboutlogins_in_tab();
 
+	  } 
+	  // right-click
+	  else if(event.button=='1') {
+		
+		if(middleclick_opens_old_pw_manager && appversion < 77) open_old_pw_manager();
+		else open_aboutlogins_in_tab();
+
+	  }
+	  
 	},
 	
 	onCreated: function(button) {
@@ -37,6 +75,7 @@ try {
   
   /* set icon */
   var button_icon = 'chrome://browser/skin/login.svg';
+  if (parseInt(Services.appinfo.version) < 68) button_icon = 'chrome://browser/skin/connection-secure.svg';
   
   // style button icon
   var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
