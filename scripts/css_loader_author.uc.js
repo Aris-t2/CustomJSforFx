b083aa4eb9746eab95a373000ee28db6e628c8ba
Aris-t2 <aris-addons@gmx.net>
2022-07-28 17:40:46 +0200

    Update css_loader_author.uc.js

--- scripts/css_loader_author.uc.js
+++ scripts/css_loader_author.uc.js
@@ -3,27 +3,32 @@
 
 /* Create 'author_style_sheet.css' file inside 'chrome' folder.
 Paste CSS code, that can not be loaded on user/agent level, into it.
-
 e.g.
-
 #tabbrowser-arrowscrollbox::part(scrollbutton-up), 
 #tabbrowser-arrowscrollbox::part(scrollbutton-down) {
   display: none !important; 
 }
-
 */
 
-var CSS_LoaderAS = {
-  init: async function() {
-    var my_author_css_file = await IOUtils.readUTF8(OS.Path.join(OS.Constants.Path.profileDir, 'chrome', 'author_style_sheet.css'));
+Components.utils.import("resource://gre/modules/Services.jsm");
 
-    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(my_author_css_file));
+// replace comments with custom css code
+var my_code = `
+	/* Paste any CSS code here. */
+	/* Make sure your code is not inside any "@-moz-document / url / url-prefix" queries or it might not work. */
+	`;
+
+var CSS_Loader_AS = {
+ init: function() {
+	
+	var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+	var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent(my_code), null, null);
 
 	// remove old style sheet
 	if (sss.sheetRegistered(uri,sss.AUTHOR_SHEET)) sss.unregisterSheet(uri,sss.AUTHOR_SHEET);
-	sss.loadAndRegisterSheet(uri, sss.AUTHOR_SHEET);  
-  }
-}
+	sss.loadAndRegisterSheet(uri, sss.AUTHOR_SHEET);
 
-CSS_LoaderAS.init();
+ }
+};
+
+CSS_Loader_AS.init();
