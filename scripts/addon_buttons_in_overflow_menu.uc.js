a65e38218c9c8482b0f3f174a3514d91b659b7a3
Aris-t2 <aris-addons@gmx.net>
2023-03-27 17:38:47 +0200

    Update addon_buttons_in_overflow_menu.uc.js

--- scripts/addon_buttons_in_overflow_menu.uc.js
+++ scripts/addon_buttons_in_overflow_menu.uc.js
@@ -1,13 +1,22 @@
-// 'Add-on buttons in overflow menu' script for Firefox 111+ by Aris
-//
-// At least one default toolbar button has to be inside overflow menu for it to show up on navigation toolbar.
-// Pin buttons to toolbar or move buttons to overflow menu using 'right-click' context menus 'Pin to Toolbar'.
-// Unified extension button gets hidden and moved to toolbars end for extension popups to appear there.
+/* 'Add-on buttons in overflow menu' script for Firefox 111+ by Aris
+
+  At least one default toolbar button has to be inside overflow menu for it to show up on navigation toolbar.
+  Pin buttons to toolbar or move buttons to overflow menu using 'right-click' context menus 'Pin to Toolbar'.
+  Unified extension button gets hidden and moved to toolbars end for extension popups to appear there.
+  
+  [!] Experimental: code might...
+  ... work on multiple windows (Fx 113 Nightly)
+  ... work on first window only (Fx 111 release)
+  ... not work at all (Fx 112 Beta)
+*/
 
 
 (function() {
 
-	document.getElementById("widget-overflow-fixed-list").appendChild(document.getElementById("unified-extensions-area"));
+	try {
+	  document.getElementById("widget-overflow-fixed-list").appendChild(document.getElementById("unified-extensions-area"));
+	} catch (e) {}
+	
   
 	// style sheet
 	Components.classes['@mozilla.org/content/style-sheet-service;1'].getService(Components.interfaces.nsIStyleSheetService).loadAndRegisterSheet(
