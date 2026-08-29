2f4d88c4dd507d2f1273e28c50fe90826366e1e3
Sneakpeakcss <77424331+Sneakpeakcss@users.noreply.github.com>
2026-08-29 10:03:07 +0200

    Update tabstoolbar_below_navigation_toolbar.uc.js
    Fixes: #69

--- scripts/tabstoolbar_below_navigation_toolbar.uc.js
+++ scripts/tabstoolbar_below_navigation_toolbar.uc.js
@@ -1,9 +1,12 @@
 // this moves tabs toolbar below navigation toolbar without tweaking any cosmetics
 // for OS titlebar only
-// works with Firefox 134+
+// works with Firefox 153+
 
 
-(function(){
-  var tabbar = document.getElementById("TabsToolbar");
-  tabbar.parentNode.appendChild(tabbar);
+(function() {
+  let tabbar = document.getElementById('TabsToolbar');
+  let notifbar = document.getElementById('notifications-toolbar');
+  if (tabbar && notifbar && !tabbar.collapsed) {
+    notifbar.parentNode.insertBefore(tabbar, notifbar);
+  }
 })();
